9ff3e0abefffec1069e26b27f2a7f29da4f81e7e
Johnny Carlo <timothyjhask@gmail.com>
2016-08-27 17:12:09 -0400

    Added files to repository

---
+++ Array.cpp
@@ -0,0 +1,89 @@
+// $Id: Array.cpp 820 2011-01-17 15:30:41Z hillj $
+
+// Honor Pledge:
+//
+// I pledge that I have neither given nor receieved any help
+// on this assignment.
+
+#include "Array.h"
+
+Array::Array (void)
+{
+
+}
+
+Array::Array (size_t length)
+{
+
+}
+
+Array::Array (size_t length, char fill)
+{
+
+}
+
+
+Array::Array (const Array & array)
+{
+
+}
+
+Array::~Array (void)
+{
+
+}
+
+const Array & Array::operator = (const Array & rhs)
+{
+
+}
+
+char & Array::operator [] (size_t index)
+{
+
+}
+
+const char & Array::operator [] (size_t index) const
+{
+
+}
+
+char Array::get (size_t index) const
+{
+
+}
+
+void Array::set (size_t index, char value)
+{
+
+}
+
+void Array::resize (size_t new_size)
+{
+
+}
+
+int Array::find (char ch) const
+{
+
+}
+
+int Array::find (char ch, size_t start) const
+{
+
+}
+
+bool Array::operator == (const Array & rhs) const
+{
+
+}
+
+bool Array::operator != (const Array & rhs) const
+{
+
+}
+
+void Array::fill (char ch)
+{
+
+}
---
+++ Array.inl
@@ -0,0 +1,16 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor received any help
+// on this assignment.
+
+inline
+size_t Array::size (void) const
+{
+  return 0;
+}
+
+inline
+size_t Array::max_size (void) const
+{
+  return 0;
+}
